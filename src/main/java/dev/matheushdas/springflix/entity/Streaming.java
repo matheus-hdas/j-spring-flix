938862cb44a1901aacd1cc4312b0ec79ce5faf08
matheus-hdas <matheussantos.betim@gmail.com>
2024-11-03 19:23:31 -0300

    feat: add `Streaming` class and `StreamingDTO`

---
+++ src/main/java/dev/matheushdas/springflix/entity/Streaming.java
@@ -0,0 +1,18 @@
+package dev.matheushdas.springflix.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_streaming")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Streaming {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
