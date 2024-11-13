3255077cd7755b2a843fb6b0cdf6396333d4fbbe
matheus-hdas <matheussantos.betim@gmail.com>
2024-11-12 23:27:47 -0300

    feat: add `User` entity

---
+++ src/main/java/dev/matheushdas/springflix/entity/User.java
@@ -0,0 +1,27 @@
+package dev.matheushdas.springflix.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
