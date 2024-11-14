2a5cfff65cb14fe6a2062cc6dbcdde5771ce09f0
matheus-hdas <matheussantos.betim@gmail.com>
2024-11-14 13:49:03 -0300

    refactor: create `CreateMovieRequest`, `UpdateMovieRequest` and `MovieResponse` dtos for a better code organization and responsability management

---
+++ src/main/java/dev/matheushdas/springflix/dto/UpdateMovieRequest.java
@@ -0,0 +1,19 @@
+package dev.matheushdas.springflix.dto;
+
+import dev.matheushdas.springflix.entity.Category;
+import dev.matheushdas.springflix.entity.Streaming;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record UpdateMovieRequest(Long id,
+                                 String title,
+                                 String description,
+                                 LocalDate releaseDate,
+                                 Double rating,
+                                 LocalDateTime createdAt,
+                                 LocalDateTime updatedAt,
+                                 List<Category> categories,
+                                 List<Streaming> streamings) {
+}
