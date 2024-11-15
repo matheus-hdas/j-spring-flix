28b623694ff023bc12b1c99b52dde2e2d4a508e7
matheus-hdas <matheussantos.betim@gmail.com>
2024-11-15 11:13:38 -0300

    feat: add `login` feature with JWT token

--- src/main/java/dev/matheushdas/springflix/controller/AuthController.java
+++ src/main/java/dev/matheushdas/springflix/controller/AuthController.java
@@ -1,15 +1,14 @@
 package dev.matheushdas.springflix.controller;
 
+import dev.matheushdas.springflix.dto.LoginRequest;
+import dev.matheushdas.springflix.dto.LoginResponse;
 import dev.matheushdas.springflix.dto.RegisterRequest;
 import dev.matheushdas.springflix.dto.RegisterResponse;
 import dev.matheushdas.springflix.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -21,5 +20,10 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest user) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest userCredentials) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.login(userCredentials));
     }
 }
--- src/main/java/dev/matheushdas/springflix/service/AuthService.java
+++ src/main/java/dev/matheushdas/springflix/service/AuthService.java
@@ -1,16 +1,25 @@
 package dev.matheushdas.springflix.service;
 
+import dev.matheushdas.springflix.config.jwt.TokenProvider;
+import dev.matheushdas.springflix.dto.LoginRequest;
+import dev.matheushdas.springflix.dto.LoginResponse;
 import dev.matheushdas.springflix.dto.RegisterRequest;
 import dev.matheushdas.springflix.dto.RegisterResponse;
 import dev.matheushdas.springflix.entity.User;
 import dev.matheushdas.springflix.mapper.UserMapper;
 import dev.matheushdas.springflix.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthService {
+public class AuthService implements UserDetailsService {
 
     @Autowired
     private UserRepository repository;
@@ -19,11 +28,31 @@ public class AuthService {
     private UserMapper mapper;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenProvider tokenProvider;
 
     public RegisterResponse save(RegisterRequest user) {
         User encrypted = mapper.toEntity(user);
-        encrypted.setPassword(passwordEncoder.encode(encrypted.getPassword()));
+        encrypted.setPassword(encoder.encode(encrypted.getPassword()));
         return mapper.toResponse(repository.save(encrypted));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findUserByEmail(username).orElseThrow();
+    }
+
+    public LoginResponse login(LoginRequest userCredentials) {
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(userCredentials.email(), userCredentials.password());
+        Authentication authenticate = manager.authenticate(userAndPass);
+
+        User user = (User) authenticate.getPrincipal();
+
+        return new LoginResponse(tokenProvider.generateToken(user));
     }
 }
