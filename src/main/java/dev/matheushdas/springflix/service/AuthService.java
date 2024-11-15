f7844c9ec89daaf178a3b9d6a211e7dfe51354e2
matheus-hdas <matheussantos.betim@gmail.com>
2024-11-15 10:29:24 -0300

    feat: add password encoding to register function

--- src/main/java/dev/matheushdas/springflix/service/AuthService.java
+++ src/main/java/dev/matheushdas/springflix/service/AuthService.java
@@ -2,9 +2,11 @@ package dev.matheushdas.springflix.service;
 
 import dev.matheushdas.springflix.dto.RegisterRequest;
 import dev.matheushdas.springflix.dto.RegisterResponse;
+import dev.matheushdas.springflix.entity.User;
 import dev.matheushdas.springflix.mapper.UserMapper;
 import dev.matheushdas.springflix.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +18,12 @@ public class AuthService {
     @Autowired
     private UserMapper mapper;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public RegisterResponse save(RegisterRequest user) {
-        return mapper.toResponse(repository.save(mapper.toEntity(user)));
+        User encrypted = mapper.toEntity(user);
+        encrypted.setPassword(passwordEncoder.encode(encrypted.getPassword()));
+        return mapper.toResponse(repository.save(encrypted));
     }
 }
